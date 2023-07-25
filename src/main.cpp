0ff873b312b187137346932b568464d528f64349
Lukasz Jakubek <lukasz.jakubek@gmail.com>
2023-07-25 21:30:02 +0200

    Ota and other changes

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,22 @@
 #define LGFX_AUTODETECT // Autodetect board
 #define LGFX_USE_V1     // set to use new version of library
 // #define LV_CONF_INCLUDE_SIMPLE
-
-/* Uncomment below line to draw on screen with touch */
-// #define DRAW_ON_SCREEN
-
 #include <LovyanGFX.hpp> // main library
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <lvgl.h>
+#include "lv_conf.h"
+#include <ArduinoOTA.h>
+#define ESP8266
+#include <AHT10.h>
+#include <Wire.h>
+uint8_t readStatus = 0;
+
+AHT10 myAHT15(AHT10_ADDRESS_0X38, AHT15_SENSOR);
+
 //#include "D:\Users\LukaszJ\Documents\PlatformIO\Projects\LVGL8-WT32-SC01-Arduino\.pio\libdeps\esp32dev\lvgl\src\core\lv_obj_style.h"
 static LGFX lcd;         // declare display variable
 
-#include <WiFi.h>
-#include <PubSubClient.h>
 String ssid = "ILMMJ-TRI";
 String password = "majaimichal";
 String mqtt_server = "192.168.1.17";
@@ -22,8 +28,6 @@ String clientId = "temperatury/Bathroom";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-#include <lvgl.h>
-#include "lv_conf.h"
 /*** Setup screen resolution for LVGL ***/
 static const uint16_t screenWidth = 480;
 static const uint16_t screenHeight = 320;
@@ -125,6 +129,181 @@ void callback(char *topic, byte *payload, unsigned int length)
   }
 }
 
+/*** Display callback to flush the buffer to screen ***/
+void display_flush(lv_disp_drv_t * disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+  lcd.startWrite();
+  lcd.setAddrWindow(area->x1, area->y1, w, h);
+  lcd.pushColors((uint16_t *)&color_p->full, w * h, true);
+  lcd.endWrite();
+
+  lv_disp_flush_ready(disp);
+}
+
+/*** Touchpad callback to read the touchpad ***/
+void touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data)
+{
+  uint16_t touchX, touchY;
+  bool touched = lcd.getTouch(&touchX, &touchY);
+
+  if (!touched)
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_PR;
+
+    /*Set the coordinates*/
+    data->point.x = touchX;
+    data->point.y = touchY;
+
+    // Serial.printf("Touch (x,y): (%03d,%03d)\n",touchX,touchY );
+  }
+}
+
+/* Counter button event handler */
+static void counter_event_handler(lv_event_t * e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t *btn = lv_event_get_target(e);
+  if (code == LV_EVENT_CLICKED)
+  {
+    static uint8_t cnt = 0;
+    cnt++;
+
+    /*Get the first child of the button which is the label and change its text*/
+    lv_obj_t *label = lv_obj_get_child(btn, 0);
+    lv_label_set_text_fmt(label, "Button: %d", cnt);
+    LV_LOG_USER("Clicked");
+    Serial.println("Clicked");
+  }
+}
+
+/* Toggle button event handler */
+static void toggle_event_handler_2(lv_event_t * e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+  if (code == LV_EVENT_VALUE_CHANGED)
+  {
+    LV_LOG_USER("Toggled");
+    Serial.println("Toggled");
+    if((state1 = !state1))
+      client.publish("temperatury/Bathroom/Button1", "1");
+    else
+      client.publish("temperatury/Bathroom/Button1", "0");
+  }
+}
+
+/* Toggle button event handler */
+static void toggle_event_handler_3(lv_event_t * e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+  if (code == LV_EVENT_VALUE_CHANGED)
+  {
+    LV_LOG_USER("Toggled");
+    Serial.println("Toggled");
+    if((state2 = !state2))
+      client.publish("temperatury/Bathroom/Button2", "1");
+    else
+      client.publish("temperatury/Bathroom/Button2", "0");
+  }
+}  
+
+static void checkbox_event_handler(lv_event_t * e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+  if (code == LV_EVENT_VALUE_CHANGED)
+  {
+    LV_LOG_USER("Check Toggled");
+    Serial.println("check Toggled");
+  }
+}
+
+static void switch_event_handler(lv_event_t * e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+  if (code == LV_EVENT_VALUE_CHANGED)
+  {
+    LV_LOG_USER("Switch toggled");
+    Serial.println("Switch toggled");
+  }
+}
+
+static void slider_event_handler(lv_event_t * e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+  if (code == LV_EVENT_VALUE_CHANGED)
+  {
+    lv_obj_t *slider = lv_event_get_target(e);
+    lv_label_set_text_fmt(label_slider, "%" LV_PRId32, lv_slider_get_value(slider));
+    lv_obj_align_to(label_slider, slider, LV_ALIGN_OUT_TOP_MID, 0, -15); /*Align top of the slider*/
+  }
+}
+
+void lv_button_demo(void)
+{
+  lv_obj_t *label_b1,*label_b2,*label_b3;
+
+  // Button with counter
+  lv_obj_t *btn1 = lv_btn_create(lv_scr_act());
+  lv_obj_add_event_cb(btn1, counter_event_handler, LV_EVENT_ALL, NULL);
+
+  lv_obj_set_pos(btn1, 20, 20);   /*Set its position*/
+  lv_obj_set_size(btn1, 250, 50); /*Set its size*/
+
+  label_b1 = lv_label_create(btn1);
+  lv_label_set_text(label_b1, "Button");
+  lv_obj_center(label_b1);
+
+  // Toggle button
+  lv_obj_t *btn2 = lv_btn_create(lv_scr_act());
+  lv_obj_add_event_cb(btn2, toggle_event_handler_2, LV_EVENT_ALL, NULL);
+  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_set_pos(btn2, 20, 80);   /*Set its position*/
+  lv_obj_set_size(btn2, 250, 50); /*Set its size*/
+
+  label_b2 = lv_label_create(btn2);
+  lv_label_set_text(label_b2, "Toggle Button");
+  lv_obj_center(label_b2);
+
+  // Toggle button
+  lv_obj_t *btn3 = lv_btn_create(lv_scr_act());
+  lv_obj_add_event_cb(btn3, toggle_event_handler_3, LV_EVENT_ALL, NULL);
+  lv_obj_add_flag(btn3, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_set_pos(btn3, 20, 140);   /*Set its position*/
+  lv_obj_set_size(btn3, 250, 50); /*Set its size*/
+
+  label_b3 = lv_label_create(btn3);
+  lv_label_set_text(label_b3, "Toggle Button");
+  lv_obj_center(label_b3);
+
+  lv_obj_t *cb1 = lv_checkbox_create(lv_scr_act());
+  lv_obj_add_event_cb(cb1, checkbox_event_handler, LV_EVENT_ALL, NULL);
+  lv_obj_add_flag(cb1, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_set_pos(cb1, 250, 210); /*Set its position*/
+  lv_obj_set_size(cb1, 120, 50); /*Set its size*/
+  lv_checkbox_set_text_static(cb1, "Fan ON");
+
+  lv_obj_t *sw1 = lv_switch_create(lv_scr_act());
+  lv_obj_add_event_cb(sw1, switch_event_handler, LV_EVENT_ALL, NULL);
+  lv_obj_add_flag(sw1, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_set_pos(sw1, 50, 210); /*Set its position*/
+
+  lv_obj_t *sl1 = lv_slider_create(lv_scr_act());
+  lv_obj_add_event_cb(sl1, slider_event_handler, LV_EVENT_ALL, NULL);
+  lv_slider_set_range(sl1, 0, 100);
+  lv_obj_set_pos(sl1, 20, 280); /*Set its position*/
+
+  label_slider = lv_label_create(sl1);
+  lv_label_set_text(label_slider, "0");                           // set label text
+  lv_obj_align_to(label_slider, sl1, LV_ALIGN_TOP_RIGHT, 0, -15); /*Align top of the slider*/
+}
+
+
 void setup(void)
 {
   Serial.begin(115200); /* prepare for possible serial debug */
@@ -187,11 +366,47 @@ void setup(void)
   client.setServer(mqtt_server.c_str(), 1883);
   client.setCallback(callback);
   reconnect();
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println(F("AHT15 setup atempt"));
+  while (myAHT15.begin(4, 2) != true)
+  {
+    Serial.println(F("AHT15 not connected or fail to load calibration coefficient")); //(F()) save string to flash & keeps dynamic memory free
+    delay(5000);
+  }
   lv_button_demo();
 }
 
 void loop()
 {
+  ArduinoOTA.handle();  
   lv_timer_handler(); /* let the GUI do its work */
   delay(5);
   if (!client.connected())
@@ -199,165 +414,5 @@ void loop()
     reconnect();
   }
   client.loop();
+}
 
-#ifdef DRAW_ON_SCREEN
-  /*** Draw on screen with touch ***/
-  if (lcd.getTouch(&x, &y))
-  {
-    lcd.fillRect(x - 2, y - 2, 5, 5, TFT_RED);
-    lcd.setCursor(380, 0);
-    lcd.printf("Touch:(%03d,%03d)", x, y);
-    // }
-#endif
-  }
-
-  /*** Display callback to flush the buffer to screen ***/
-  void display_flush(lv_disp_drv_t * disp, const lv_area_t *area, lv_color_t *color_p)
-  {
-    uint32_t w = (area->x2 - area->x1 + 1);
-    uint32_t h = (area->y2 - area->y1 + 1);
-
-    lcd.startWrite();
-    lcd.setAddrWindow(area->x1, area->y1, w, h);
-    lcd.pushColors((uint16_t *)&color_p->full, w * h, true);
-    lcd.endWrite();
-
-    lv_disp_flush_ready(disp);
-  }
-
-  /*** Touchpad callback to read the touchpad ***/
-  void touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data)
-  {
-    uint16_t touchX, touchY;
-    bool touched = lcd.getTouch(&touchX, &touchY);
-
-    if (!touched)
-    {
-      data->state = LV_INDEV_STATE_REL;
-    }
-    else
-    {
-      data->state = LV_INDEV_STATE_PR;
-
-      /*Set the coordinates*/
-      data->point.x = touchX;
-      data->point.y = touchY;
-
-      // Serial.printf("Touch (x,y): (%03d,%03d)\n",touchX,touchY );
-    }
-  }
-
-  /* Counter button event handler */
-  static void counter_event_handler(lv_event_t * e)
-  {
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t *btn = lv_event_get_target(e);
-    if (code == LV_EVENT_CLICKED)
-    {
-      static uint8_t cnt = 0;
-      cnt++;
-
-      /*Get the first child of the button which is the label and change its text*/
-      lv_obj_t *label = lv_obj_get_child(btn, 0);
-      lv_label_set_text_fmt(label, "Button: %d", cnt);
-      LV_LOG_USER("Clicked");
-      Serial.println("Clicked");
-    }
-  }
-
-  /* Toggle button event handler */
-  static void toggle_event_handler(lv_event_t * e)
-  {
-    lv_event_code_t code = lv_event_get_code(e);
-    if (code == LV_EVENT_VALUE_CHANGED)
-    {
-      LV_LOG_USER("Toggled");
-      Serial.println("Toggled");
-      if(state1 = !state1)
-        client.publish("temperatury/Bathroom/Button1", "1");
-      else
-        client.publish("temperatury/Bathroom/Button1", "0");
-    }
-  }
-
-  static void checkbox_event_handler(lv_event_t * e)
-  {
-    lv_event_code_t code = lv_event_get_code(e);
-    if (code == LV_EVENT_VALUE_CHANGED)
-    {
-      LV_LOG_USER("Check Toggled");
-      Serial.println("check Toggled");
-      if(state2 = !state2)
-        client.publish("temperatury/Bathroom/Button2", "1");
-      else
-        client.publish("temperatury/Bathroom/Button2", "0");
-    }
-  }
-
-  static void switch_event_handler(lv_event_t * e)
-  {
-    lv_event_code_t code = lv_event_get_code(e);
-    if (code == LV_EVENT_VALUE_CHANGED)
-    {
-      LV_LOG_USER("Switch toggled");
-      Serial.println("Switch toggled");
-    }
-  }
-
-  static void slider_event_handler(lv_event_t * e)
-  {
-    lv_event_code_t code = lv_event_get_code(e);
-    if (code == LV_EVENT_VALUE_CHANGED)
-    {
-      lv_obj_t *slider = lv_event_get_target(e);
-      lv_label_set_text_fmt(label_slider, "%" LV_PRId32, lv_slider_get_value(slider));
-      lv_obj_align_to(label_slider, slider, LV_ALIGN_OUT_TOP_MID, 0, -15); /*Align top of the slider*/
-    }
-  }
-
-  void lv_button_demo(void)
-  {
-    lv_obj_t *label;
-
-    // Button with counter
-    lv_obj_t *btn1 = lv_btn_create(lv_scr_act());
-    lv_obj_add_event_cb(btn1, counter_event_handler, LV_EVENT_ALL, NULL);
-
-    lv_obj_set_pos(btn1, 20, 20);   /*Set its position*/
-    lv_obj_set_size(btn1, 250, 50); /*Set its size*/
-
-    label = lv_label_create(btn1);
-    lv_label_set_text(label, "Button");
-    lv_obj_center(label);
-
-    // Toggle button
-    lv_obj_t *btn2 = lv_btn_create(lv_scr_act());
-    lv_obj_add_event_cb(btn2, toggle_event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-    lv_obj_set_pos(btn2, 20, 80);   /*Set its position*/
-    lv_obj_set_size(btn2, 250, 50); /*Set its size*/
-
-    lv_label_set_text(label, "Toggle Button");
-    lv_obj_center(label);
-
-    lv_obj_t *cb1 = lv_checkbox_create(lv_scr_act());
-    lv_obj_add_event_cb(cb1, checkbox_event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_add_flag(cb1, LV_OBJ_FLAG_CHECKABLE);
-    lv_obj_set_pos(cb1, 250, 170); /*Set its position*/
-    lv_obj_set_size(cb1, 120, 50); /*Set its size*/
-    lv_checkbox_set_text_static(cb1, "Fan ON");
-
-    lv_obj_t *sw1 = lv_switch_create(lv_scr_act());
-    lv_obj_add_event_cb(sw1, switch_event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_add_flag(sw1, LV_OBJ_FLAG_CHECKABLE);
-    lv_obj_set_pos(sw1, 50, 210); /*Set its position*/
-
-    lv_obj_t *sl1 = lv_slider_create(lv_scr_act());
-    lv_obj_add_event_cb(sl1, slider_event_handler, LV_EVENT_ALL, NULL);
-    lv_slider_set_range(sl1, 0, 100);
-    lv_obj_set_pos(sl1, 20, 280); /*Set its position*/
-
-    label_slider = lv_label_create(sl1);
-    lv_label_set_text(label_slider, "0");                           // set label text
-    lv_obj_align_to(label_slider, sl1, LV_ALIGN_TOP_RIGHT, 0, -15); /*Align top of the slider*/
-  }
